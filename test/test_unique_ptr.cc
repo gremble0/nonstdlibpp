d4b7f4a046ee1259ae27421e054ab80be651e3b2
gremble0 <hermanstornes@gmail.com>
2025-07-12 17:12:42 +0200

    Add move and move assignemnt unique_ptr tests

--- test/test_unique_ptr.cc
+++ test/test_unique_ptr.cc
@@ -21,3 +21,23 @@ TEST_CASE("Test make_unique") {
         REQUIRE(heap_complex_type->v == nullptr);
     }
 }
+
+TEST_CASE("Test unique_ptr rule of 5") {
+    SECTION("Test destructor") {}
+
+    // Copy constructor and copy assignment operator are deleted
+
+    SECTION("Test move constructor") {
+        auto heap_int = nstd::make_unique<int>(2);
+        nstd::unique_ptr<int> moved(std::move(heap_int));
+        REQUIRE(heap_int.get() == nullptr);
+        REQUIRE(*moved.get() == 2);
+    }
+
+    SECTION("Test move assignment operator") {
+        auto heap_int = nstd::make_unique<int>(2);
+        nstd::unique_ptr<int> moved = std::move(heap_int);
+        REQUIRE(heap_int.get() == nullptr);
+        REQUIRE(*moved.get() == 2);
+    }
+}
