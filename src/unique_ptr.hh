36444473b28adb6124d65b053f5421dc34f46c47
gremble0 <hermanstornes@gmail.com>
2025-07-12 17:07:06 +0200

    Add some remaining accessors and swap method unique_ptr

--- src/unique_ptr.hh
+++ src/unique_ptr.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <utility>
+
 namespace nstd {
 
 template <typename T> struct default_delete {
@@ -30,10 +31,19 @@ template <typename T, typename Deleter = default_delete<T>> class unique_ptr {
     constexpr unique_ptr &operator=(unique_ptr &&other) noexcept {
         reset(other.get());
         other.m_ptr = nullptr;
+
         return *this;
     }
 
-    constexpr pointer operator->() const noexcept { return m_ptr; }
+    constexpr ~unique_ptr() { m_deleter(m_ptr); }
+
+    [[nodiscard]] constexpr pointer get() const { return m_ptr; }
+
+    [[nodiscard]] constexpr deleter get_deleter() const { return m_deleter; }
+
+    constexpr pointer operator->() const noexcept { return get(); }
+
+    constexpr explicit operator bool() const noexcept { return get() != nullptr; }
 
     constexpr void reset(pointer ptr) noexcept {
         // In case deleter throws, do the deletion last. STL has a contract where custom deleters should not throw, but
@@ -49,9 +59,10 @@ template <typename T, typename Deleter = default_delete<T>> class unique_ptr {
         return ptr;
     }
 
-    constexpr ~unique_ptr() { m_deleter(m_ptr); }
-
-    constexpr pointer get() const { return m_ptr; }
+    constexpr void swap(unique_ptr &other) noexcept {
+        m_deleter = std::exchange(m_deleter, other.get_deleter());
+        m_ptr = std::exchange(m_ptr, other.get());
+    }
 
   private:
     [[no_unique_address]] deleter m_deleter{};
