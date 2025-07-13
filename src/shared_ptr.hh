7a0949edff4566d572e04735618be476c0dcc4ea
gremble0 <hermanstornes@gmail.com>
2025-07-13 17:39:29 +0200

    Fix copy constructor for shared_ptr

--- src/shared_ptr.hh
+++ src/shared_ptr.hh
@@ -2,7 +2,7 @@
 
 #include <atomic>
 #include <cassert>
-#include <cstddef>
+#include <cstdint>
 
 namespace nstd {
 
@@ -28,13 +28,15 @@ class shared_count_base {
 
     constexpr void weak_release() noexcept { --m_weak_count; }
 
+    [[nodiscard]] constexpr int64_t use_count() const noexcept { return m_ref_count; }
+
     shared_count_base(const shared_count_base &other) = delete;
     shared_count_base &operator=(const shared_count_base &other) = delete;
 
   private:
     // TODO(gremble0): maybe some smarter locking - here we have to lock and unlock twice when strong releasing
-    std::atomic<size_t> m_ref_count = 1;
-    std::atomic<size_t> m_weak_count = 1;
+    std::atomic<int64_t> m_ref_count = 1;
+    std::atomic<int64_t> m_weak_count = 1;
 };
 
 class shared_count {
@@ -43,7 +45,7 @@ class shared_count {
 
     constexpr explicit shared_count(shared_count_base *count) noexcept : m_count(count) { assert(m_count); }
 
-    shared_count(const shared_count &other) {
+    constexpr shared_count(const shared_count &other) noexcept {
         if (other.m_count != nullptr) {
             m_count = other.m_count;
             m_count->add_reference();
@@ -62,6 +64,8 @@ class shared_count {
 
     shared_count &operator=(shared_count &&other) = delete;
 
+    [[nodiscard]] constexpr int64_t use_count() const noexcept { return m_count == nullptr ? 0 : m_count->use_count(); }
+
   private:
     shared_count_base *m_count{nullptr};
 };
@@ -70,11 +74,14 @@ template <typename T> class shared_ptr {
   public:
     using pointer = T *;
 
+    template <typename U, typename... Args> friend constexpr shared_ptr<U> make_shared(Args... args);
+
     constexpr shared_ptr() noexcept = default;
 
     constexpr ~shared_ptr() noexcept = default;
 
-    constexpr explicit shared_ptr(pointer ptr) noexcept : m_ptr(ptr) {}
+    // TODO(gremble0): noexcept
+    constexpr explicit shared_ptr(pointer ptr) : shared_ptr(ptr, shared_count{new shared_count_base}) {}
 
     constexpr shared_ptr(const shared_ptr &other) noexcept = default;
 
@@ -83,15 +90,26 @@ template <typename T> class shared_ptr {
     constexpr shared_ptr &operator=(const shared_ptr &other) noexcept = delete;
     constexpr shared_ptr &operator=(shared_ptr &&other) noexcept = delete;
 
-    constexpr pointer get() { return m_ptr; }
+    constexpr pointer get() const { return m_ptr; }
+
+    constexpr pointer operator->() const noexcept { return get(); }
+
+    constexpr T &operator*() const noexcept { return *get(); }
+
+    [[nodiscard]] constexpr int64_t use_count() const noexcept { return m_count.use_count(); }
 
   private:
+    // Used by make shared
+    constexpr explicit shared_ptr(pointer ptr, const shared_count &count) noexcept : m_ptr(ptr), m_count(count) {
+        assert(m_ptr);
+    }
+
     pointer m_ptr{nullptr};
     shared_count m_count{};
 };
 
 template <typename T, typename... Args> constexpr shared_ptr<T> make_shared(Args... args) {
-    return shared_ptr(new T{std::forward<Args>(args)...});
+    return shared_ptr(new T{std::forward<Args>(args)...}, shared_count{new shared_count_base});
 }
 
 } // namespace nstd
--- test/test_shared_ptr.cc
+++ test/test_shared_ptr.cc
@@ -3,6 +3,12 @@
 #include <catch2/catch_test_macros.hpp>
 #include <memory>
 
+struct complex_type {
+    int x;
+    int y;
+    void *v;
+};
+
 TEST_CASE("Test shared_ptr size is same as STL") {
     STATIC_REQUIRE(sizeof(std::shared_ptr<int>) == sizeof(nstd::shared_ptr<int>));
 }
@@ -14,9 +20,35 @@ TEST_CASE("Test make_shared") {
     }
 
     SECTION("Test for complex types") {
-        auto heap_complex_type = nstd::make_unique<complex_type>(1, 2, nullptr);
+        auto heap_complex_type = nstd::make_shared<complex_type>(1, 2, nullptr);
         REQUIRE(heap_complex_type->x == 1);
         REQUIRE(heap_complex_type->y == 2);
         REQUIRE(heap_complex_type->v == nullptr);
     }
+}
+
+TEST_CASE("Test shared_ptr rule of 5") {
+    SECTION("Test destructor") {}
+
+    SECTION("Test copy constructor") {
+        auto heap_int = nstd::make_shared<int>(2);
+        // NOLINTNEXTLINE(performance-unnecessary-copy-initialization)
+        auto copied(heap_int);
+        REQUIRE(heap_int.use_count() == 2);
+        REQUIRE(copied.use_count() == 2);
+    }
+
+    // SECTION("Test move constructor") {
+    //     auto heap_int = nstd::make_shared<int>(2);
+    //     nstd::shared_ptr<int> moved(std::move(heap_int));
+    //     REQUIRE(heap_int.get() == nullptr);
+    //     REQUIRE(*moved == 2);
+    // }
+    //
+    // SECTION("Test move assignment operator") {
+    //     auto heap_int = nstd::make_shared<int>(2);
+    //     nstd::shared_ptr<int> moved = std::move(heap_int);
+    //     REQUIRE(heap_int.get() == nullptr);
+    //     REQUIRE(*moved == 2);
+    // }
 }
